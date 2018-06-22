f3540b5262803583d25118075f5dfabed4e3615d
JoshHerrala <josh@unionbayproject.com>
2018-06-21 20:51:42 -0400

    rather lame buttons

--- app/assets/javascripts/components/post.jsx
+++ app/assets/javascripts/components/post.jsx
@@ -9,6 +9,6 @@ window.Post = createReactClass({
 // or, equivalent:
 class Post extends React.Component {
   render() {
-    return <h1>{this.props.title}</h1>
+    return <h2>{this.props.title}</h2>
   }
 }
